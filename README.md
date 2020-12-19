4c672ef7372029b00dc888021ec926163b783e7b
madziazzzz <76257178+madziazzzz@users.noreply.github.com>
2020-12-19 16:31:34 +0100

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Otwórz tę stronę na [https://madziazzzz.github.io/test/](https://madziazzzz.github.io/test/)
+
+## Użyj jako rozszerzenia
+
+This repository can be added as an **extension** in MakeCode.
+
+* otwórz [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* kliknij na **Nowy Projekt**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/madziazzzz/test** and import
+
+## Edytuj ten projekt ![Status kompilacji](https://github.com/madziazzzz/test/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* otwórz [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* wklej **https://github.com/madziazzzz/test** i kliknij importuj
+
+## Podgląd bloków
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![Renderowany widok bloków](https://github.com/madziazzzz/test/raw/master/.github/makecode/blocks.png)
+
+#### Metadane (używane do wyszukiwania, renderowania)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,61 @@
+input.onButtonPressed(Button.A, function () {
+    stop = 0
+    while (stop != 1) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    stop += 1
+})
+input.onButtonPressed(Button.B, function () {
+    stop = 0
+    while (stop != 1) {
+        music.playMelody("C5 B A G F E D C ", 120)
+    }
+})
+let stop = 0
+stop = 0
+basic.forever(function () {
+	
+})
